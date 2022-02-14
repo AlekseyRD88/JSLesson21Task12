c7f34747a9edc1eacea3b36d850ead2a19632da3
= <neymegen50@gmail.com>
2022-02-14 15:32:13 +0200

    message

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 export function squaredNumbers() {
-  let searchForNumber = document.querySelector('.number');
-  let squaredNumber = searchForNumber.dataset.number;
-  squaredNumber = squaredNumber * squaredNumber;
-  document.querySelector('li').setAttribute('data-squared-number', 'squaredNumber');
+  let searchForNumber = document.querySelectorAll('.number');
+  let newArr = searchForNumber.dataset.number.forEach((x) => x * x);
+  li.setAttribute('data-squared-number', [...newArr]);
 }
