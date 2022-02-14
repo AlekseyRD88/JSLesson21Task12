0f37225f588e7d5e90d54c10f82cac5743ca52ec
= <neymegen50@gmail.com>
2022-02-14 15:36:33 +0200

    message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export function squaredNumbers() {
   let searchForNumber = document.querySelectorAll('.number');
   let newArr = searchForNumber.dataset.number.forEach((x) => x * x);
-  li.setAttribute('data-squared-number', [...newArr]);
+  searchForNumber.setAttribute('data-squared-number', [...newArr]);
 }
