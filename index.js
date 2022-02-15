6d5a57bff2992e2b3883885f029629f0c6b83a4e
= <neymegen50@gmail.com>
2022-02-15 12:58:32 +0200

    newry

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 export function squaredNumbers() {
   let searchForNumber = document.querySelectorAll('.number');
   searchForNumber.forEach((x) => x.dataset.number * x.dataset.number);
-  let liDomElement = document.querySelectorAll('li');
-  liDomElement.forEach((x, i) = x.setAttribute('data-squared-number', 'searchForNumber[i]'));
+  let liDomElement = document.querySelectorAll('li').forEach((x, i) = x.setAttribute('data-squared-number', 'searchForNumber[i]'));
 }
