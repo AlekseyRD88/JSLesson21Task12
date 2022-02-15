15d1cd150240497dc145d0c5caa9a2d46e0ddda5
= <neymegen50@gmail.com>
2022-02-15 13:21:31 +0200

    message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export function squaredNumbers() {
   let searchForNumber = document.querySelectorAll('.number');
   searchForNumber.forEach((x) => x.dataset.number * x.dataset.number);
-  let newAttribute = document.querySelectorAll('li').forEach((liDomElement, i) => liDomElement.setAttribute('data-squared-number', 'searchForNumber[i]'));
+  let newAttribute = document.querySelectorAll('li').forEach((liDomElement, i) => liDomElement.setAttribute('data-squared-number', searchForNumber[i]));
 }
