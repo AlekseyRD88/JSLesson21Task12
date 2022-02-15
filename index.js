63d22b8f6274819deaf7c6d4f2a313e14edf4327
= <neymegen50@gmail.com>
2022-02-15 15:26:32 +0200

    message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export function squaredNumbers() {
-  let searchForNumber = document.querySelectorAll('.number');
-  searchForNumber.forEach((x) => x.dataset.number);
-  searchForNumber.forEach((x) => x * x);
-  let newAttribute = document.querySelectorAll('li').forEach((liDomElement, i) => liDomElement.setAttribute('data-squared-number', 'searchForNumber[i]'));
+  const searchForNumber = document.querySelectorAll('.number');
+  searchForNumber.map((x) => x.dataset.number);
+  searchForNumber.map((x) => x * x);
+  let newAttribute = document.querySelectorAll('li').map((liDomElement, i) => liDomElement.setAttribute('data-squared-number', 'searchForNumber[i]'));
 }
