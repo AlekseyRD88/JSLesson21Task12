30c2528e6cd37c8b26bd31536698e99e90ff0226
= <neymegen50@gmail.com>
2022-02-15 12:53:47 +0200

    message4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export function squaredNumbers() {
   let searchForNumber = document.querySelectorAll('.number');
   searchForNumber.forEach((x) => x.dataset.number * x.dataset.number);
-  let newAttribute = document.querySelectorAll('li').forEach((x, i) = x.setAttribute('data-squared-number', searchForNumber[i]));
+  let liDomElement = document.querySelectorAll('li');
+  liDomElement.forEach((x, i) = x.setAttribute('data-squared-number', searchForNumber[i]));
 }
