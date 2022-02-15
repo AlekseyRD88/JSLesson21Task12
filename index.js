34afcc49a09192040f6ef4af218914b72da308ed
= <neymegen50@gmail.com>
2022-02-15 12:55:30 +0200

    mess

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ export function squaredNumbers() {
   let searchForNumber = document.querySelectorAll('.number');
   searchForNumber.forEach((x) => x.dataset.number * x.dataset.number);
   let liDomElement = document.querySelectorAll('li');
-  liDomElement.forEach((x, i) = x.setAttribute('data-squared-number', searchForNumber[i]));
+  liDomElement.forEach((x, i) = x.setAttribute('data-squared-number', 'searchForNumber[i]'));
 }
