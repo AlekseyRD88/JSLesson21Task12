c3b0df51d85be24b74a203fee7d4856b0e26f80a
= <neymegen50@gmail.com>
2022-02-15 19:13:22 +0200

    message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export function squaredNumbers() {
   const searchForNumber = document.querySelectorAll('.number');
-  searchForNumber.map((x) => x.dataset.number);
-  searchForNumber.map((x) => x * x);
-  let newAttribute = document.querySelectorAll('li').map((liDomElement, i) => liDomElement.setAttribute('data-squared-number', 'searchForNumber[i]'));
+  searchForNumber.forEach((elem) => {
+    const number = elem.dataset.number;
+    elem.dataset.squaredNumber = number ** 2;
+  });
 }
